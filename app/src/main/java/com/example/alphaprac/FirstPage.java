b59f9e24beae0baf24e838b99488b5e50ce77741
Chintu <bcsf18m017@pucit.edu.pk>
2021-11-05 11:59:50 +0500

    First Page With Repo Link

--- app/src/main/java/com/example/alphaprac/FirstPage.java
+++ app/src/main/java/com/example/alphaprac/FirstPage.java
@@ -2,13 +2,29 @@ package com.example.alphaprac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstPage extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        btn=(Button)findViewById(R.id.repoLink);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/bcsf18m017/Quiz-App-"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
